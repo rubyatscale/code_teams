c4cced2bd4cd037446328bc75f0828371ea69a8a
Valerie Burzynski <valerie.burzynski@gusto.com>
2025-05-29 14:17:14 -0500

    refactor: use longform definition of modules over shorthand

--- lib/code_teams/utils.rb
+++ lib/code_teams/utils.rb
@@ -1,15 +1,17 @@
-module CodeTeams::Utils
-  extend self
+module CodeTeams
+  module Utils
+    extend self
 
-  def underscore(string)
-    string.gsub('::', '/')
-      .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-      .gsub(/([a-z\d])([A-Z])/, '\1_\2')
-      .tr('-', '_')
-      .downcase
-  end
+    def underscore(string)
+      string.gsub('::', '/')
+        .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+        .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+        .tr('-', '_')
+        .downcase
+    end
 
-  def demodulize(string)
-    string.split('::').last
+    def demodulize(string)
+      string.split('::').last
+    end
   end
 end
