f18d7b633fa026d263326239e4d05dab4f1988b8
Joseph Sak <joseph.sak@gusto.com>
2025-05-30 15:30:43 -0600

    Ruby 3.2 maintenance

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'bundler/setup'
 require 'pry'
 require 'code_teams'
 
-Dir[File.expand_path('support/**/*.rb', __dir__)].sort.each { |f| require f }
+Dir[File.expand_path('support/**/*.rb', __dir__)].each { |f| require f }
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
