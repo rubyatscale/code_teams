ab35360bfda8c7b04f3a598a49740c6c8c774d10
Joseph Sak <joseph.sak@gusto.com>
2026-01-23 13:04:23 -0700

    feat: add RSpec helper integration
    Provides an opt-in RSpec helper (code_team_with_config) and per-example cleanup
    for in-memory teams.

---
+++ lib/code_teams/rspec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+#
+# typed: false
+
+require 'securerandom'
+require 'code_teams/testing'
+
+module CodeTeams
+  module RSpecHelpers
+    def code_team_with_config(team_config = {})
+      team_config = team_config.dup
+      team_config[:name] ||= "Fake Team #{SecureRandom.hex(4)}"
+      CodeTeams::Testing.create_code_team(team_config)
+    end
+  end
+end
+
+if defined?(RSpec)
+  RSpec.configure do |config|
+    config.include CodeTeams::RSpecHelpers
+
+    config.around do |example|
+      example.run
+      # Bust caches because plugins may hang onto stale data between examples.
+      if CodeTeams::Testing.code_teams.any?
+        CodeTeams.bust_caches!
+        CodeTeams::Testing.reset!
+      end
+    end
+  end
+end
---
+++ spec/lib/code_teams/r_spec_helpers_integration_spec.rb
@@ -0,0 +1,14 @@
+require 'code_teams/rspec'
+
+RSpec.describe CodeTeams::RSpecHelpers do
+  it 'exposes code_team_with_config and makes the team discoverable' do
+    code_team_with_config(name: 'RSpec Team')
+
+    expect(CodeTeams.find('RSpec Team')).not_to be_nil
+  end
+
+  it 'cleans up testing teams between examples' do
+    expect(CodeTeams::Testing.code_teams).to be_empty
+    expect(CodeTeams.find('RSpec Team')).to be_nil
+  end
+end
