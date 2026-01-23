988b333353c5f6fcc9416579a51b92c080a1c1ba
Joseph Sak <joseph.sak@gusto.com>
2026-01-23 13:32:21 -0700

    test: remove redundant before/after blocks
    The enable! around hook handles cleanup automatically.

--- spec/lib/code_teams/testing_spec.rb
+++ spec/lib/code_teams/testing_spec.rb
@@ -3,16 +3,6 @@ require 'code_teams/testing'
 CodeTeams::Testing.enable!
 
 RSpec.describe CodeTeams::Testing do
-  before do
-    CodeTeams.bust_caches!
-    described_class.reset!
-  end
-
-  after do
-    CodeTeams.bust_caches!
-    described_class.reset!
-  end
-
   describe '.create_code_team' do
     it 'adds the team to CodeTeams.all and CodeTeams.find' do
       team = described_class.create_code_team({ name: 'Temp Team', extra_data: { foo: { bar: 1 } } })
